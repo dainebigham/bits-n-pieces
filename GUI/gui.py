71f0844ad51ffd460a77a1661ff0a874905392d7
Daine Bigham <daine.bigham@gmail.com>
2021-06-14 17:56:36 +1200

    playing around with *args and **kwargs

---
+++ GUI/gui.py
@@ -0,0 +1,11 @@
+# import tkinter as tk
+
+# window = tk.Tk()
+
+# window.title('Python GUI')
+# window.minsize(width=500, height=300)
+
+# lable = tk.Label(text='Label', font=('courier', 12, 'bold'))
+# lable.pack(side='left')
+
+# window.mainloop()
